Fixing units, trying to debug Gurobi inputs and commenting renovation planner

Core/main_beers.py
@@ -478,16 +478,20 @@ def load_param(combinations):
     logger.info('Loading data for Basopra Optimization')
 
     series = combinations['hh']['series']
+    idx = series.index
+
     attributes = combinations['hh']['attributes']
     heat_pump = combinations['hh']['heat_pump']
-    df_el = series[['ElectricConsumption', 'SolarPVProduction','dhw']]
-    pv_roof_capacity = attributes['roof_pv_capacity']
-    pv_wall_capacity = attributes['wall_pv_capacity']
-    pv_capacity = pv_roof_capacity + pv_wall_capacity
-    elec_price = attributes['elec_price']/100
+    heat_pump.series.index = idx
+    df_el = series[['ElectricConsumption', 'SolarPVProduction','dhw']]  # kWh, kWh, kWh
+
+    pv_roof_capacity = attributes['roof_pv_capacity']  # kW
+    pv_wall_capacity = attributes['wall_pv_capacity']  # kW
+    pv_capacity = pv_roof_capacity + pv_wall_capacity  # kW
+    elec_price = attributes['elec_price']/100  # frs/kWh
     ev_profiles = combinations['hh']['ev_profiles']
     
-    Export_price = 0.06
+    Export_price = 0.06 # frs/kWh
     
     logger.info("Choose the corresponding profile for electricity")
 
@@ -512,33 +516,46 @@ def load_param(combinations):
 
     week = 1
 
-
     df_el.columns=['E_demand','E_PV','dhw']
     
     # PV_nom = df_el.E_PV.sum()/1000 # Estimated kW
-    PV_nom = np.round(pv_capacity/1000,1)
+    # PV_nom = np.round(pv_capacity/1000,1)
+    PV_nom = pv_capacity
     logger.info('PV_nom : {PV_nom}')
     # Let-s try with the demand instead of the PV due to the high PV nom in the facade
     param["Capacity"] = np.round(df_el.E_demand.sum()/1000,0) # pv_capacity['Roof'] + pv_capacity['Wall']# Capacity is for the battery, PV_nom is for PV
     logger.info('Battery Capacity : {Capacity}')
     param['Inverter_power'] = round(pv_capacity/1000/ 1.2, 1)
-
+    # if heat_pump is not None:
+    #     df_heat_new = heat_pump.series[[
+    #         'Set_T', 
+    #         'Temp', 
+    #         'Req_kWh', 
+    #         'Temp_supply', 
+    #         'Temp_supply_tank',
+    #         'COP_SH',
+    #         'COP_tank',
+    #         'COP_DHW',
+    #         'hp_sh_cons',
+    #         'hp_tank_cons',
+    #         'hp_dhw_cons',
+    #     ]]
 
     df_heat_new = heat_pump.series[[
-        'Set_T', 
-        'Temp', 
-        'Req_kWh', 
-        'Temp_supply', 
-        'Temp_supply_tank',
-        'COP_SH',
-        'COP_tank',
-        'COP_DHW',
-        'hp_sh_cons',
-        'hp_tank_cons',
-        'hp_dhw_cons',
+            'Set_T', 
+            'Temp', 
+            'Req_kWh', 
+            'Temp_supply', 
+            'Temp_supply_tank',
+            'COP_SH',
+            'COP_tank',
+            'COP_DHW',
+            'hp_sh_cons',
+            'hp_tank_cons',
+            'hp_dhw_cons',
     ]]
     #df_heat_new = df_heat_new.reset_index(drop=True)         # Remove the datetime index
-    ev_param, df_EVs = load_multi_EV_data(ev_profiles, param, df_heat_new.index)
+    ev_param, df_EVs = load_multi_EV_data(ev_profiles, param, idx)
     #if ev_profiles is not None:
     #    ev_param, df_EVs = load_multi_EV_data(ev_profiles,param)
 
@@ -573,7 +590,7 @@ def load_param(combinations):
         f"{ev}_{col}"
         for ev, col in df_EVs.columns
     ]
-    df_EVs.index=df_heat_new.index
+    df_EVs.index=idx
     '''else:
         cols = ['E_EV_req','E_EV_trip','EV_home','EV_away']
         df_EVs = pd.DataFrame(
@@ -582,13 +599,18 @@ def load_param(combinations):
         )
         df_EVs.index = df_heat_new.index'''
 
-    df_el.index = df_heat_new.index
-    
+    df_el.index = idx    
+
     df_el['Price_flat']=elec_price
     df_el['Export_price']=Export_price
     df_el.rename(columns={'dhw': 'Req_kWh_DHW'}, inplace=True)
-    df_el['Req_kWh_DHW']/=10
-    data_input=pd.concat([df_el,df_heat_new,df_EVs],axis=1,copy=True,sort=False)
+    # df_el['Req_kWh_DHW']/=10
+    to_concat = [df_el]
+    # if heat_pump is not None:
+    #     to_concat.append(df_heat_new)
+    to_concat.append(df_heat_new)
+    to_concat.append(df_EVs)
+    data_input=pd.concat(to_concat,axis=1,copy=True,sort=False)
 
     #skip the first DHW data since cannot be produced simultaneously with SH    data_input.loc[(data_input.index.hour<2),'Req_kWh_DHW']=0
     #data_input.loc[:,'E_PV']=data_input.loc[:,'E_PV']*PV_nom
@@ -621,6 +643,8 @@ def load_param(combinations):
         'App_comb': create_app_combinations(combinations['App_comb']),
     })
 
+    print(data_input.head())
+    print(data_input[['E_demand', 'E_PV', 'Price_flat']].describe())
     return param, data_input
 
 
@@ -699,6 +723,8 @@ def do_battery_only_simulation(b_data: Dict[str, Any]):
         'ElectricConsumption': series['ElectricConsumption'],
         'SolarPVProduction': series['SolarPVProduction'],
     })
+
+    efficiency = core_config.param_load_fixed_parameters.Converter_efficiency_batt
 
     df['E_PV_load'] = 0.0
     df['E_PV_batt'] = 0.0
@@ -783,14 +809,18 @@ def run_basopra_simulation(big_data_object):
             basic_simulations_indexes.append(Combs_todo_dicts.index(combination))
 
     basic_simulations = []
-    for i in basic_simulations_indexes:
-        basic_simulations.append(Combs_todo_dicts.pop(i))
+    for sim in sorted(basic_simulations_indexes, reverse=True):
+        basic_simulations.append(Combs_todo_dicts.pop(sim))
 
     # Running non gurobi simulations
-    non_gurobi_results = [
-        special_configurations(combination)
-        for combination in basic_simulations
-    ]
+    for i in range(len(basic_simulations)):
+        building_id = basic_simulations[i]['combinations']['name']
+        conf_id = basic_simulations[i]['combinations']['conf']
+        basopra_results[(building_id, conf_id)] = {
+            'simulation_inputs': basic_simulations[i]['combinations'],
+            'simulation_outputs': basic_simulations[conf_id](basic_simulations[i]),
+        }
+
     # Running gurobi simulations
     results = run_parallel(
         pooling2,

Core/renovation_planner.py
@@ -19,6 +19,15 @@ planner_config = config.renovation_planning
 np.random.seed(planner_config.seed)
 
 async def load_from_api(save_file: str) -> pd.DataFrame:
+    """
+    Loads all buildings found and the attributes we need
+
+    Args:
+        save_file (str): File to save the retrieved buildings 
+
+    Returns:
+        pd.DataFrame: buildings list with their attributes
+    """
     api_wrapper = await ApiWrapper.from_config(config['openbeers_address'])
     zones: List[Zone] = []
     buildings: Dict[int, Building] = {} 
@@ -57,10 +66,19 @@ async def load_from_api(save_file: str) -> pd.DataFrame:
     return df
 
 def estimate_vehicles_per_building(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Estimates the number of vehicles per building based on some Swiss household trends
+
+    Args:
+        df (pd.DataFrame): List of buildings and their attributes
+
+    Returns:
+        pd.DataFrame: Same as input list with an estimated number of vehicles per building added
+    """
     people_per_houselhold = planner_config.people_per_household
     vehicles_per_household = planner_config.vehicles_per_household
     inhabitants_per_building = df['num_occupants']
-    # CAUTION: assumption made that a one person building still counts as a household
+    # NOTE: assumption made that a one person building still counts as a household
     households_per_building = np.maximum(1, np.round(inhabitants_per_building / people_per_houselhold).astype(int))
     total_households = households_per_building.sum()
     total_vehicles = round(total_households * vehicles_per_household)
@@ -82,6 +100,14 @@ def estimate_vehicles_per_building(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 def add_evs(df: pd.DataFrame) -> pd.DataFrame:
+    """Takes building vehicles and determines which are EVs for each year of interest
+
+    Args:
+        df (pd.DataFrame): List of all buildings and their attributes
+
+    Returns:
+        pd.DataFrame: Same as input list with number of electric vehicle for each year added per building
+    """
     vehicle_counts = df['total_vehicle_count']
     ev_ratios = planner_config.ev_inclusion_rate
     building_ids = list(vehicle_counts.keys())
@@ -137,12 +163,30 @@ def add_evs(df: pd.DataFrame) -> pd.DataFrame:
     return buildings_df
 
 def add_current_HP(df: pd.DataFrame) -> pd.DataFrame:
+    """Add Heat Pumps for buildings where a HP has already been installed
+
+    Args:
+        df (pd.DataFrame): List of all buildings found with attributes
+
+    Returns:
+        pd.DataFrame: Same list as input with a flag added if a HP exists in 2025 according to available data
+    """
     df['hp_installed_2025'] = df["gwaerzh1"].isin([7410, 7411])
-    # NOTE CAUTION. assumption made that gwaerzh2 is not useful as it is generally for annex buildings and such
+    # NOTE: assumption made that gwaerzh2 is not useful as it is generally for annex buildings and such
     return df
 
 def add_HP_when_renovated(df: pd.DataFrame, years_of_interest: List[int]) -> pd.DataFrame:
-    # CAUTION: Assumption made that any absent renovation date is due to the renovation being exceedingly old
+    """Adds a heat pump to buildings if their current heating installation has become older than 20 years, at 
+    each year of interest, and should have been renovated
+
+    Args:
+        df (pd.DataFrame): List of buildings and their attributes
+        years_of_interest (List[int]): List of years at which the age of the heating installation should be checked
+
+    Returns:
+        pd.DataFrame: List of buildings with HP presence tags added/modified for each year of interest
+    """
+    # NOTE: Assumption made that any absent renovation date is due to the renovation being exceedingly old
     district_heating_presence = df["gwaerzh1"].isin([7460, 7461])
     renovation_dates = pd.to_datetime(df['gwaerdath1'], format = "%d.%m.%Y", errors='coerce')
     dummy_date = pd.Timestamp("1900-01-01")
@@ -166,6 +210,18 @@ def add_HP_when_renovated(df: pd.DataFrame, years_of_interest: List[int]) -> pd.
     return buildings_df
 
 def add_HP_to_projected_ratio(df: pd.DataFrame, years_of_interest: List[int]) -> pd.DataFrame:
+    """
+    NOT USED (Does not align with VS renovation policies)
+    Adds heat pumps to buildings according to a projection study of HP penetration.
+    Heat Pumps are added until their penetration over a Municipality matches the study's projection
+
+    Args:
+        df (pd.DataFrame): List of buildings and their attributes
+        years_of_interest (List[int]): List of years at which the age of the heating installation should be checked
+
+    Returns:
+        pd.DataFrame: List of buildings with HP presence tags added/modified for each year of interest
+    """
     hp_inclusion_rates = dataframe_load(planner_config.hp_inclusion_rates)
     hp_inclusion_rates = hp_inclusion_rates[
         (hp_inclusion_rates['Canton'] == planner_config.canton) &
@@ -196,6 +252,17 @@ def add_HP_to_projected_ratio(df: pd.DataFrame, years_of_interest: List[int]) ->
     return full_df
 
 def add_HP(df: pd.DataFrame) -> pd.DataFrame:
+    """Main Heat pump addition function that adds HPs to buildings using sub functions:
+        - add_current_HP 
+        - add_HP_when_renovated
+        - add_HP_to_projected_ratio
+
+    Args:
+        df (pd.DataFrame): List of buildings and their attributes
+
+    Returns:
+        pd.DataFrame: List of buildings with added HPs
+    """
     years_of_interest = planner_config.years_of_interest
     df['commune'] = df["commune"].str.replace(r"\s*\(.*?\)", "", regex=True)
 
@@ -204,9 +271,6 @@ def add_HP(df: pd.DataFrame) -> pd.DataFrame:
         (hp_inclusion_rates['Canton'] == planner_config.canton) &
         (hp_inclusion_rates['quantile'] == planner_config.quantile)
     ]
-
-    # municipalities = df["commune"].unique()
-    # municipalities_hp_inclusion_rates = hp_inclusion_rates[hp_inclusion_rates['MunicipalityName'].isin(municipalities)]
 
     # existing hp and district heating
     df = add_current_HP(df)
@@ -221,26 +285,52 @@ def add_HP(df: pd.DataFrame) -> pd.DataFrame:
     return df 
 
 def prep_battery_prob(df: pd.DataFrame) -> pd.DataFrame:
+    """Predetermines if a building will have a battery installed if PV is lated installed
+
+    Args:
+        df (pd.DataFrame): List of buildings and their attributes
+
+    Returns:
+        pd.DataFrame: List of buildings with added flag determining it they receive a battery when PV is installed
+    """
     will_have_battery_if_PV = np.random.rand(len(df)) <= planner_config.battery_install_ratio
     df["install_battery"] = will_have_battery_if_PV
     return df
 
     
 class RenovationPlanning:
+    """
+    Renovation planning class used at the start of the Basopra process to add attributes to buildings based on CitySim outputs
+    """
     def __init__(self, renovation_plan_path: str) -> None:
+        """Loads the renovation plan created by this file's main fct. The renovation plan is created once but loaded before every
+        Basopra simulation so as to always have the same attributes determined created for each building
+
+        Args:
+            renovation_plan_path (str): Location of the renovation plan
+        """
         self.renovation_plan: pd.DataFrame = dataframe_load(renovation_plan_path, 0)
     
     def add_EV_counts(self, buildings: Dict[int, Any], simulation: Simulation) -> None:
+        """Adds their estimated ev_counts to each building based on the year of the simulation
+
+        Args:
+            buildings (Dict[int, Any]): buildings (id, attributes) 
+            simulation (Simulation): Simulation object from OpenBeers
+        """
         sim_year = simulation.year
-        # ev_counts_year = self.renovation_plan[f'ev_count_{sim_year}']
         for b, values in buildings.items():
             ev_count = 0
             if b in self.renovation_plan.index:
-                ev_count = 1 #self.renovation_plan.loc[b, f'ev_count_{sim_year}']
-
+                ev_count = self.renovation_plan.loc[b, f'ev_count_{sim_year}']
             values['attributes']['ev_count'] = ev_count
 
     def add_batteries(self, buildings: Dict[int, Any]) -> None:
+        """Adds batteries to each buidling based on the presence of PV and the battery installation flag from the renovation planning
+
+        Args:
+            buildings (Dict[int, Any]): buildings (id, attributes) 
+        """
         surfaces = ['roof', 'wall', 'floor']
         battery_type = planner_config.battery_type
         for b, values in buildings.items():
@@ -256,6 +346,15 @@ class RenovationPlanning:
                 values['battery'] = battery
     
     def add_HP_flags(self, buildings: Dict[int, Any], simulation: Simulation) -> None:
+        """Adds a heat pump to all buildings that have been marked to have a heat pump at the given simulation year
+        If CitySim marked a building to have a HP, it is added but other HPs are removed so that the HP penetration ratio 
+        stays unchanged
+
+        Args:
+            buildings (Dict[int, Any]): buildings (id, attributes) 
+            simulation (Simulation): Simulation object from OpenBeers
+        """
+        rng = np.random.default_rng(seed=simulation.id)
         sim_year = simulation.year
         citysim_hp_flag_name = planner_config.citysim_hp_flag
         citysim_hp = {}
@@ -280,7 +379,7 @@ class RenovationPlanning:
         number_of_flags_to_change = len(flags_to_turn_True)
         number_of_ok_differences = len(ok_but_different)
         print(number_of_flags_to_change, number_of_ok_differences)
-        flags_to_turn_False = np.random.choice(ok_but_different, size=number_of_flags_to_change, replace=False)
+        flags_to_turn_False = rng.choice(ok_but_different, size=number_of_flags_to_change, replace=False)
 
         for b, values in buildings.items():
             has_HP = self.renovation_plan.loc[b, f'hp_installed_{sim_year}']
@@ -291,6 +390,9 @@ class RenovationPlanning:
             values['attributes']['has_HP'] = has_HP
 
 async def main() -> None:
+    """
+    Main function estimating the presence of EVs, HPs and Batteries at given years of interest and saving them in a file
+    """
     save_file = planner_config.save_file
 
     df = None