754ce3e60a7f65ce4ac2b3677f0ca7b28d474ea5
ltroillet <lucien.troillet@hevs.ch>
2025-07-18 17:31:38 +0200

    future proofing KPI code

--- Core/kpi_calculator.py
+++ Core/kpi_calculator.py
@@ -1,12 +1,17 @@
 import sys
 import os
-from typing import List, Tuple
+from typing import Any, Dict, List, Tuple
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import pandas as pd
 from pandas import DataFrame
 from utils.utils import dataframe_load, is_type, list_files_recursive
+import logging
+
+from utils.logger import logger
+logger.setLevel(logging.WARNING)
+
 
 # TODO modify for timestamp based index when available and normalize outputs by dt
 def assume_month_range() -> List[Tuple[int, int]]:
@@ -20,9 +25,15 @@ def assume_month_range() -> List[Tuple[int, int]]:
     return month_ranges
 
 def autoconsumption(df: DataFrame):
+    e_pv = df["E_PV"].sum()
+    if e_pv == 0:
+        return 0
     return df["E_PV_load"].sum() / df["E_PV"].sum()
 
 def autarky(df: DataFrame):
+    e_demand = df["E_demand"].sum()
+    if e_demand == 0:
+        return 0
     return df["E_PV_load"].sum() / df["E_demand"].sum()
 
 def peak_consumption(df: DataFrame):
@@ -52,47 +63,75 @@ def calc_kpis(df: DataFrame):
         kpis[kpi] = method(df)
     return pd.Series(kpis)
 
-def get_building_kpis(df: DataFrame):
+def get_building_monthly_kpis(df: DataFrame):
     month_ranges = assume_month_range()
     monthly_kpis = []
     for i, (start, end) in enumerate(month_ranges, 1):
         month_df = df.iloc[start:end]
         kpis = calc_kpis(month_df)
-        kpis['month'] = i
+        kpis['month'] = int(i)
         monthly_kpis.append(kpis)
-    return monthly_kpis
+    df_monthly_kpis = pd.DataFrame(monthly_kpis)
+    df_monthly_kpis['month'] = df_monthly_kpis['month'].astype(int)
+    return df_monthly_kpis
+
+# def get_building_monthly_kpis(building_df) -> DataFrame:
+#     simulation_kpis: List = []
+#     for file in dir_content['files']:
+#         if is_type(file, '.csv'):
+#             df = dataframe_load(f'{dir_name}/{file}')
+#             kpis = get_building_kpis(df)
+#             simulation_kpis.append(kpis)
+#     return simulation_kpis
+
+
+def get_all_building_dfs(main_dir:str) -> Dict[str, Any]:
+    # TODO, once we have date timestamped item. Add date sorting to the mix
+    structure = list_files_recursive(main_dir)
+    flattened_data = []
+    for directory in structure:
+        for file in structure[directory]['files']:
+            if is_type(file, '.csv'):
+                df = dataframe_load(f'{main_dir}/{directory}/{file}')
+                split_file_name = file.split('.')[0].split('_')
+                building = split_file_name[1]
+                conf = split_file_name[2]
+                df['simulation'] = directory
+                df['building'] = building
+                df['configuration'] = conf
+                flattened_data.append(df)
+    final_df = pd.concat(flattened_data, ignore_index=True)
+    return final_df 
 
 def main():
     main_dir = 'outputs_basopra'
-    structure = list_files_recursive(main_dir)
-    for directory in structure:
-        print(directory)
-        for file in structure[directory]['files']:
-            if is_type(file, '.csv'):
-                print(file)
-                df = dataframe_load(f'{main_dir}/{directory}/{file}')
-                kpis = get_building_kpis(df)
-                
+    zone_dfs = get_all_building_dfs(main_dir)
+    monthly_kpi_dfs = pd.DataFrame()
+    yearly_kpi_dfs = []
+    for sim_name, simulation in zone_dfs.groupby('simulation'):
+        print(sim_name)
+        print(simulation.shape)
+        print(simulation['building'].unique().shape)
+        for b_name, building in simulation.groupby('building'):
+            for conf_name, conf in building.groupby('configuration'):
+                month_kpis = get_building_monthly_kpis(conf)
+                month_kpis['simulation'] = sim_name
+                month_kpis['building'] = b_name
+                month_kpis['configuration'] = conf_name
+                monthly_kpi_dfs = pd.concat([monthly_kpi_dfs, month_kpis], ignore_index=True)
 
-    file_name = 'outputs_basopra/val_de_bagnes_41_climate_contemporary_pv_roof_2025/df_902138_only Battery.csv'
-    df = dataframe_load(file_name)
-    month_ranges = assume_month_range()
-    monthly_kpis = []
-    for i, (start, end) in enumerate(month_ranges, 1):
-        month_df = df.iloc[start:end]
-        kpis = calc_kpis(month_df)
-        kpis['month'] = i
-        monthly_kpis.append(kpis)
-    monthly_kpis_df = pd.DataFrame(monthly_kpis)
-    monthly_kpis_df['month'] = monthly_kpis_df['month'].astype(int)
-    monthly_kpis_df = monthly_kpis_df.set_index('month')
-    yearly_kpis = calc_kpis(df)
+                yearly_kpis = calc_kpis(conf)
+                yearly_kpis['simulation'] = sim_name
+                yearly_kpis['building'] = b_name
+                yearly_kpis['configuration'] = conf_name
+                yearly_kpi_dfs.append(yearly_kpis)
+    yearly_kpi_dfs = pd.DataFrame(yearly_kpi_dfs)
 
     pd.set_option("display.float_format", "{:.2f}".format)
 
-    print(monthly_kpis_df)
-    print(yearly_kpis)
-    print(df.describe())
+    print(monthly_kpi_dfs)
+    print(yearly_kpi_dfs)
+    # print(df.describe())
 
 if __name__ == "__main__":
     main()
